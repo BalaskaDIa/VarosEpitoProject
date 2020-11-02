cfb1b4299fea1ed2136afbbaca93990c1b2efdea
BalaskaDIa <balaska.klaudiaa@gmail.com>
2020-11-02 20:21:41 +0100

    2. Feladat városok kiíratása, fájl beolvasása

--- VarosEpitoProject/Program.cs
+++ VarosEpitoProject/Program.cs
@@ -14,7 +14,26 @@ namespace VarosEpitoProject
             varosok.Add(new Varos("Budapest", 3));
             varosok.Add(new Varos("Washington", 1));
             varosok.Add(new Varos("London", 2));
+
+            try
+            {
+                using (var sr = new StreamReader("varosok.csv"))
+                {
+                    Console.WriteLine(sr.ReadToEnd());
+                    string[] st = sr.ReadLine().Split(';');
+                    varosok.Add(new Varos(st[0], Convert.ToInt32(st[1])));
+
+                }
+                varosok.Sort((x, y) => x.Alapterulet.CompareTo(y.Alapterulet));
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("A fájl nem olvasható!");
+                Console.WriteLine(e.Message);
+            }
+
         }
+    }
     }
 }
     
