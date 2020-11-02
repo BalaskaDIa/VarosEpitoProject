b0b51a85d6248934dbf89e841a3f5d24c922d310
BalaskaDIa <balaska.klaudiaa@gmail.com>
2020-11-02 20:09:54 +0100

    1. Feladat c.) getter propertyk átalakítása, MaxLakosok, Alapterulet hozzáadása

--- VarosEpitoProject/Varos.cs
+++ VarosEpitoProject/Varos.cs
@@ -11,9 +11,74 @@ namespace VarosEpitoProject
         int hazak;
         int uzletek;
 
-        public string Nev { get => nev; set => nev = value; }
-        public int Lakosok { get => lakosok; set => lakosok = value; }
-        public int Hazak { get => hazak; set => hazak = value; }
-        public int Uzletek { get => uzletek; set => uzletek = value; }
+        public string Nev
+        {
+            get
+            {
+                return this.nev;
+            }
+        }
+        public int Lakosok
+        {
+            get
+            {
+                return this.lakosok;
+            }
+            set
+            {
+                if (this.lakosok > value)
+                {
+                    Console.WriteLine("A lakosok száma nem csökkenhet!");
+                }
+                else if (this.Lakosok > this.MaxLakosok)
+                {
+                    Console.WriteLine("A lakosok száma nem lépheti át a maximumot!");
+                }
+                else
+                {
+                    this.lakosok = value;
+                }
+            }
+        }
+
+        public int Hazak
+        {
+            get
+            {
+                return this.hazak;
+            }
+            set
+            {
+                if (this.MaxLakosok < this.lakosok)
+                {
+                    Console.WriteLine("Nem lépheti át");
+                }
+                else
+                {
+                    this.hazak = value;
+                }
+            }
+        }
+        public int Uzletek
+        {
+            get
+            {
+                return this.uzletek;
+            }
+        }
+        public int MaxLakosok
+        {
+            get
+            {
+                return this.hazak * 6;
+            }
+        }
+        public double Alapterulet
+        {
+            get
+            {
+                return this.hazak * 110 + this.uzletek * 85.5;
+            }
+        }
     }
 }
